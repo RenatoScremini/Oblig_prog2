a762b187c3c3a393f734093eee3559b65c963e9f
jpodest4 <jpodesta@correo.um.edu.uy>
2022-06-21 09:40:22 -0300

    Agregando Constructor, Setters & Getters a las Entities.

--- src/Entidades/Beer.java
+++ src/Entidades/Beer.java
@@ -2,9 +2,44 @@ package Entidades;
 
 public class Beer {
     private long id;
+
     private String name;
+
     private double abv;
 
+    public Beer(long id, String name, double abv) {
+        this.id = id;
+        this.name = name;
+        this.abv = abv;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAbv() {
+        return abv;
+    }
+
+    public void setAbv(double abv) {
+        this.abv = abv;
+    }
+
     //FIXME tiene que tener una list con todas las brewery
+
+
+
 
 }
--- src/Entidades/Review.java
+++ src/Entidades/Review.java
@@ -4,10 +4,62 @@ import java.util.Date;
 
 public class Review {
     private long id;
+
     private Date date;
+
     private double overallScore;
+
     private double aromaScore;
+
     private double flavourScore;
+
+    public Review(long id, Date date, double overallScore, double aromaScore, double flavourScore) {
+        this.id = id;
+        this.date = date;
+        this.overallScore = overallScore;
+        this.aromaScore = aromaScore;
+        this.flavourScore = flavourScore;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getOverallScore() {
+        return overallScore;
+    }
+
+    public void setOverallScore(double overallScore) {
+        this.overallScore = overallScore;
+    }
+
+    public double getAromaScore() {
+        return aromaScore;
+    }
+
+    public void setAromaScore(double aromaScore) {
+        this.aromaScore = aromaScore;
+    }
+
+    public double getFlavourScore() {
+        return flavourScore;
+    }
+
+    public void setFlavourScore(double flavourScore) {
+        this.flavourScore = flavourScore;
+    }
 
     //FIXME Tengo que agregar una liosta de beers
 
