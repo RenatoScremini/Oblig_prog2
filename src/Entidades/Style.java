83ddee3cff368e0c89f1b647736f31f26ff848fc
jpodest4 <jpodesta@correo.um.edu.uy>
2022-06-21 16:49:02 -0300

    Intentando carga de datos.

--- src/Entidades/Style.java
+++ src/Entidades/Style.java
@@ -1,7 +1,21 @@
 package Entidades;
 
 public class Style {
+
     private String name;
+
+    public Style(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     //FIXME tengo que agregar una lista de beer
 
 }
--- src/Entidades/User.java
+++ src/Entidades/User.java
@@ -2,6 +2,19 @@ package Entidades;
 
 public class User {
     private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     // FIXME tengo que agregar una lista de las reviews de cada user no estoy seugro como
 
 }
