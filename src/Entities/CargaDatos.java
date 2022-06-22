a560f0392cb0bb153c699ea6d7049734251800aa
Renato Scremini <rscremini1@correo.um.edu.uy>
2022-06-22 17:33:32 -0300

    Agreagando lo que faltaba de BST y Excep

--- src/Entities/CargaDatos.java
+++ src/Entities/CargaDatos.java
@@ -4,23 +4,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 
 public class CargaDatos {
 
-    public static void CargaDatosImp() throws IOException { // PREGUNTAR QUE ES ESTO DE IOExcepton
-        BufferedReader br = new BufferedReader(new FileReader("beer_dataset_test.txt"));
+    public CargaDatos() throws IOException { // PREGUNTAR QUE ES ESTO DE IOExcepton
+        BufferedReader br = new BufferedReader(new FileReader("beer_dataset_test.csv"));
 
         String currentLine;
-        String [] linea = currentLine.split(","):
-
+        String [] columna = currentLine.split(",");
+        br.readLine();
         while ((currentLine = br.readLine()) != null){
-            if(linea[0] == "\\"){
-                continue;
-
-                long.parse
-
+                long review_id = Long.parseLong(columna[0]);
+                long brewery_id = Long.parseLong(columna[1]);//FIXME que onda este error
+                String brewery_name = columna[2].substring(0, columna[2].length()) ;
+                Date review_time = new Date(Long.parseLong(columna[3])*1000);
+                double review_overall = Double.parseDouble(columna[4]);
+                double review_aroma = Double.parseDouble(columna[5]);
+                double review_appearance = Double.parseDouble(columna[6]);
+                double review_profilename = Double.parseDouble(columna[7]);
+                String beer_style = columna[8].substring(0, columna[8].length()) ;
+                double review_palate = Double.parseDouble(columna[9]);
+                double review_taste = Double.parseDouble(columna[10]);
+                String beer_name = columna[11].substring(0, columna[11].length()) ;
+                double beer_abv = Double.parseDouble(columna[12]);
+                long beer_beerid = Long.parseLong(columna[13]);
             }
+
 
         }
     }
-}
+
