a27feace602ed95bd4b0f20d1af3189b49fbf458
Renato Scremini <rscremini1@correo.um.edu.uy>
2022-06-24 10:19:16 -0300

    arreglando gitIgnore

--- src/Entities/main.java
+++ src/Entities/main.java
@@ -6,14 +6,40 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        CargaDatos todo = new CargaDatos();
-        todo.leerCSV("src/beer_dataset_test.csv");
+
+        boolean encendido = true;
+        boolean cargaDatosRealizada = false;
+
+        while(encendido){
+            Scanner escaneado = new Scanner(System.in);
+            int numEsc = Integer.parseInt(escaneado.nextLine());
+
+            System.out.println("Seleccione la opción que desee:");
+            System.out.println("1. Carga de datos\n" + "2. Ejecutar consultas\n" + "3. salir");
+
+            try{
+                switch (numEsc){
+                    case 1:
+                        if(!cargaDatosRealizada){
+                            cargaDatosRealizada = true;
+                            CargaDatos todo = new CargaDatos();
+                            todo.leerCSV("src/beer_dataset_test.csv");
+
+                        }else{
+                            System.out.println("Los datos ya fueron cargados");
+                        }
 
 
+                }
+
+
+            }catch (Exception e){
+                System.out.println("Ingrese una opcion correcta");
+            }
+        }
 
        /* System.out.println("Seleccione la opción que desee:");
-        System.out.println("1. Carga de datos\n" + "2. Ejecutar consultas\n" + "3. salir");
-*/
+        System.out.println("1. Carga de datos\n" + "2. Ejecutar consultas\n" + "3. salir");*/
 
     }
 }
