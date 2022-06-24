db883c9ee56cc4dcccdac7565f4acca56d950e95
Renato Scremini <rscremini1@correo.um.edu.uy>
2022-06-24 10:46:22 -0300

    Terminado manu y crando clase Consultas

--- src/Entities/main.java
+++ src/Entities/main.java
@@ -12,12 +12,12 @@ public class main {
 
         while(encendido){
             Scanner escaneado = new Scanner(System.in);
-            int numEsc = Integer.parseInt(escaneado.nextLine());
 
             System.out.println("Seleccione la opción que quiera:");
             System.out.println("1. Carga de datos\n" + "2. Ejecutar consultas\n" + "3. salir");
 
             try{
+                int numEsc = Integer.parseInt(escaneado.nextLine());
                 switch (numEsc){
                     case 1:
                         if(!cargaDatosRealizada){
@@ -29,20 +29,38 @@ public class main {
                             System.out.println("Los datos ya fueron cargados");
                         }
 
+                        break;
+
                     case 2:
                         if(cargaDatosRealizada) {
                             Scanner escanoConsulta = new Scanner(System.in);
-                            int numConsulta = Integer.parseInt(escaneado.nextLine());
+
 
                             System.out.println("Seleccione la consulta que quiere;");
                             System.out.println("1. Las 10 Cervezas con más reseñas en un año\n" + "2. Los 15 Catadores con más reseñas \n" + "3. Cantidad de reviews en un periodo de tiempo\n" + "4. Los 7 estilos de cerveza con mejor aroma\n" + "5. Las 5 cervezas con más reviews");
                             try {
+                                int numConsulta = Integer.parseInt(escaneado.nextLine());
                                 switch (numConsulta) {
                                     case 1:
+                                        Consultas consulta1 = new Consultas();
+                                        consulta1.Consulta1();
+                                        break;
                                     case 2:
+                                        Consultas consulta2 = new Consultas();
+                                        consulta2.Consulta2();
+                                        break;
                                     case 3:
+                                        Consultas consulta3 = new Consultas();
+                                        consulta3.Consulta3();
+                                        break;
                                     case 4:
+                                        Consultas consulta4 = new Consultas();
+                                        consulta4.Consulta4();
+                                        break;
                                     case 5:
+                                        Consultas consulta5 = new Consultas();
+                                        consulta5.Consulta5();
+                                        break;
 
                                     default:
                                         System.out.println("La opción ingresada es incorrecta");
@@ -54,6 +72,7 @@ public class main {
                         }else{
                             System.out.println("Los datos no fueron cargados");
                         }
+                        break;
 
 
                     case 3:
